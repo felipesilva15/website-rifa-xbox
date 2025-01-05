1b232a0e568f438b96c38b4ec3817194cf0fda6a
felipe.silva <felipe.silva@pwi.com.br>
2025-01-05 11:44:09 -0300

    feat: Melhorando página de como funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./features/card/card.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { ContactsComponent } from "./features/contacts/contacts.component";
@@ -12,7 +11,6 @@ import { HeaderComponent } from "./shared/components/header/header.component";
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     CardComponent,
     FooterComponent,
     ContactsComponent,
